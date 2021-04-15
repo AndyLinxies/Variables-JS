033a4f292ded1df42a5f6529fb60b6590b52e2ac
AndyLinxies <linxies@gmail.com>
2021-04-15 15:46:30 +0200

    var Prompt Methode

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+//Exo1
+//let prenom1 =prompt("Quel est votre prénom?");
+//alert(`Bonjour ${prenom1}`);
+//Exo2
+//let age = prompt("Quel est votre âge ?");
+//alert(`Tu as ${age} ans`);
+//Exo3
+//let prenom = prompt('Quel est votre pénom ?');
+//let nom = prompt('Quel est votre nom de famille?');
+//let nom_prenom= nom + " "+ prenom;
+//alert (nom_prenom);
+//Exo4
+let prenom = prompt('Quel est votre pénom ?');
+let nom = prompt('Quel est votre nom de famille?');
+let nomG = nom.toUpperCase();
+alert(`Bonjour ${nomG} ${prenom}`)
